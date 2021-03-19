054a9d33270543fdd4fac7b0a7ae788d85f61bfb
Renato Viegas <renato.viegas@softplan.com.br>
2021-03-19 20:14:42 -0300

    Commit inicial

---
+++ src/main/java/com/renatoviegas/personalbookorganizer/PersonalBookOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.renatoviegas.personalbookorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBookOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBookOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renatoviegas/personalbookorganizer/PersonalBookOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renatoviegas.personalbookorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBookOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
